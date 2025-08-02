860a0e61b2fbe81283a20a0d445561bd6768d103
Mar. Leska <mariales.leska@gmail.com>
2025-08-02 14:48:09 +0300

    Add routes for register, login, contact

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-//import About from './components/About/AboutSection'
+import About from './components/About/AboutSection'
 import Footer from './components/Footer'
 import Header from './components/Ηοme/Header'
 import Main from './components/Ηοme/Main'
-
+import ContactPage from './components/Contact/ContactPage'
+import RegisterPage from './components/Register/RegistrerPage'
+import LoginPage from './components/Login/LoginPage'
 
 function App() {
  
   return (
     <>
-      <Header/>
-      <Main/>
-      {/* <About/> */}
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Main/>}/>
+          <Route path="about" element={<About/>}/>
+          <Route path="contact" element={<ContactPage/>}/>
+          <Route path="users">
+            <Route path="register" element={<RegisterPage/>}/>
+          </Route>
+          <Route path="auth">
+            <Route path="login" element={<LoginPage/>}/>
+          </Route>
+        </Routes>
+        
+          
+        
+      </BrowserRouter>
       <Footer/> 
     </>
   )
