aa313bf697a72ff9c1249e362284d7d10b3e256a
Mar. Leska <mariales.leska@gmail.com>
2025-08-10 20:20:37 +0300

    Add decoded

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,13 +1,41 @@
 import type React from "react"
 import { Navigate } from "react-router-dom"
+import { jwtDecode } from "jwt-decode"
 
 type ProtectedRouteProps = {
   children: React.ReactNode
+  adminOnly?: boolean;
 }
 
-const ProtectedRoute = ( {children}: ProtectedRouteProps) => {
+type JWTPayload = {
+  role: string;
+  exp: number;
+}
+
+const ProtectedRoute = ( {children, adminOnly}: ProtectedRouteProps) => {
   const token = localStorage.getItem('accessToken')
-  return token ? <>{children}</> : <Navigate to="/auth/login"/>
+
+  if (!token) return <Navigate to="/auth/login" replace/>
+   
+  try {
+    const decoded = jwtDecode<JWTPayload>(token)
+
+    // Έλεγχος λήξης token
+    if (decoded.exp * 1000 < Date.now()) {
+      localStorage.removeItem("accessToken")
+      return <Navigate to="/auth/login" replace />
+    }
+
+    if (adminOnly && decoded.role !== "admin") {
+      return <Navigate to="/books" replace/>
+    }
+
+    return <>{children}</>
+  } catch (error) {
+    console.error("Invalid token", error)
+    localStorage.removeItem("accessToken")
+    return <Navigate to="/auth/login" replace/>
+  }
 }
 
 
