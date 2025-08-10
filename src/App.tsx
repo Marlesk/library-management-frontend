3b229092815ef759d7836ef85f6d2aef34c45893
Mar. Leska <mariales.leska@gmail.com>
2025-08-10 20:23:27 +0300

    Add new Routes for Admin

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,23 @@ import ContactPage from './components/public/contact/ContactPage'
 import RegisterPage from './components/public/register/RegistrerPage'
 import LoginPage from './components/public/login/LoginPage'
 import BooksPage from './components/user/books/BooksPage'
-import Footer from './components/Footer'
-import DefaultLayout from './components/public/DefaultLayout'
-import UserLayout from './components/user/UserLayout'
+import DefaultLayout from './components/public/menu/DefaultLayout'
+import UserLayout from './components/user/menu/UserLayout'
 import BookDetailsPage from './components/user/books/BookDetailsPage'
 import ProtectedRoute from './components/ProtectedRoute'
 import ProfilePage from './components/user/profile/ProfilePage'
 import BookHistoryPage from './components/user/history/BookHistoryPage'
+import AdminUsersPage from './components/admin/users/AdminUsersPage'
+import AdminLayout from './components/admin/AdminLayout'
+import HelpCenterPage from './components/admin/footer/HelpCenterPage'
+import PrivacyPolicyPage from './components/admin/footer/PrivacyPolicePage'
+import AdminProfilePage from './components/admin/profile/AdminProfilePage'
 
 function App() {
  
   return (
     <>
       <BrowserRouter>
-        
         <Routes>
           <Route element={<DefaultLayout/>}>
             <Route path="/" element={<Main/>}/>
@@ -31,6 +34,8 @@ function App() {
             <Route path="auth">
               <Route path="login" element={<LoginPage/>}/>
             </Route>  
+            <Route path="/help-center" element={<HelpCenterPage />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
           </Route>
 
           <Route element={<UserLayout/>}>
@@ -57,13 +62,33 @@ function App() {
               <BookHistoryPage/>
               </ProtectedRoute>
             }/>
+            <Route path="/help-center" element={<HelpCenterPage />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+            
           </Route>
+          
+          <Route element={<AdminLayout/>}>
+            <Route path="admin/users" element={
+                <ProtectedRoute adminOnly>
+                  <AdminUsersPage />
+                </ProtectedRoute>
+            }/>
+            <Route path="admin/profile" element={
+               <ProtectedRoute>
+                <AdminProfilePage/>
+               </ProtectedRoute>
+            }/>
+            <Route path="/help-center" element={<HelpCenterPage />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+          </Route>
+
         </Routes>  
 
+       
       
       </BrowserRouter>
 
-      <Footer/>
+     
     </>
   )
 }
