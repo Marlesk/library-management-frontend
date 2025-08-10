e08521b2d8bd87fcd4ad22dbeb21fd4e98a2a558
Mar. Leska <mariales.leska@gmail.com>
2025-08-10 20:19:12 +0300

    Add new folder navbar for organization

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import Logo from "../Logo"
+import React from "react"
+
+interface Props {
+  children?: React.ReactNode
+}
+
+const Navbar = ({children}: Props) => {
+ return (
+    <nav className='flex items-center justify-between px-5 bg-light-blue fixed w-full h-16 z-50'>
+      <Logo/>
+      {children}
+    </nav>
+ )
+}
+
+export default Navbar
