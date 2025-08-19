860c9de34053421dd6d21cd67971a6d981fbc0f8
Mar. Leska <mariales.leska@gmail.com>
2025-08-19 16:13:06 +0300

    add google component

--- src/components/public/login/LoginPage.tsx
+++ src/components/public/login/LoginPage.tsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom"
 import LoginImage from "./LoginImage"
 import LoginForm from "./LoginForm"
+import GoogleButton from "./GoogleButton"
+
 
 const LoginPage = () => {
   return (
     <>
       <div className="min-h-screen flex items-center justify-center bg-gray-100">
-        <div className="md:mt-0 mt-10 w-80 max-w-4xl md:w-full bg-white shadow-lg rounded-2xl overflow-hidden 
+        <div className="w-80 max-w-4xl md:w-full bg-white shadow-lg rounded-2xl overflow-hidden 
         flex flex-col md:flex-row mb-16 md:mb-0 py-5">
 
         <LoginImage/>
@@ -15,6 +17,9 @@ const LoginPage = () => {
             <h2 className="text-3xl font-semibold text-gray-800 mb-6">Welcome back</h2>
 
             <LoginForm/>
+
+            
+            <GoogleButton text="Continue with Google" />
           
             
             <p className="mt-6 text-sm text-center text-gray-600">
